c41d5c13e431a0b39bfca708fb18487257615284
hamza.rehman <hamza.rehman@cloudsolutions.com.sa>
2026-08-12 19:19:14 +0300

    ignore  infrastructure fields in device

--- IotDashboard.Application/Mappers/DeviceMapper.cs
+++ IotDashboard.Application/Mappers/DeviceMapper.cs
@@ -27,7 +27,26 @@ namespace IotDashboard.Application.Mappers
                 .ForMember(dest => dest.SubRegion, opt => opt.Ignore())
                 .ForMember(dest => dest.Zone, opt => opt.Ignore())
                 .ForMember(dest => dest.DeviceTenants, opt => opt.Ignore())
-                .ForMember(dest => dest.CustomerId, opt => opt.Ignore());
+                .ForMember(dest => dest.CustomerId, opt => opt.Ignore())
+                // Infrastructure fields are managed via PATCH /device/{id}/infrastructure only.
+                .ForMember(dest => dest.PowerSources, opt => opt.UseDestinationValue())
+                .ForMember(dest => dest.RectifierBrand, opt => opt.UseDestinationValue())
+                .ForMember(dest => dest.RectifierQty, opt => opt.UseDestinationValue())
+                .ForMember(dest => dest.RectifierCapacity, opt => opt.UseDestinationValue())
+                .ForMember(dest => dest.BatteryBrand, opt => opt.UseDestinationValue())
+                .ForMember(dest => dest.BatteryQty, opt => opt.UseDestinationValue())
+                .ForMember(dest => dest.BatteryCapacity, opt => opt.UseDestinationValue())
+                .ForMember(dest => dest.SolarBrand, opt => opt.UseDestinationValue())
+                .ForMember(dest => dest.SolarQty, opt => opt.UseDestinationValue())
+                .ForMember(dest => dest.SolarCapacity, opt => opt.UseDestinationValue())
+                .ForMember(dest => dest.GeneratorBrand, opt => opt.UseDestinationValue())
+                .ForMember(dest => dest.GeneratorQty, opt => opt.UseDestinationValue())
+                .ForMember(dest => dest.GeneratorCapacity, opt => opt.UseDestinationValue())
+                .ForMember(dest => dest.RmsSerialNumber, opt => opt.UseDestinationValue())
+                .ForMember(dest => dest.SimCardNumber, opt => opt.UseDestinationValue())
+                .ForMember(dest => dest.CamerasInstalledCount, opt => opt.UseDestinationValue())
+                .ForMember(dest => dest.AiEhsInstalled, opt => opt.UseDestinationValue())
+                .ForMember(dest => dest.AiSecurityInstalled, opt => opt.UseDestinationValue());
         }
     }
 }
